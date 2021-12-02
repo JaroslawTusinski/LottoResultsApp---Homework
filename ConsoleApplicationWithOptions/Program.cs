6ade055ad17e804cc0200f64c89b7d52cdabf57d
JaroslawTusinski <jarrodtusinski@gmail.com>
2021-12-02 18:58:20 +0100

    Init

---
+++ ConsoleApplicationWithOptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ConsoleApplicationWithOptions.Controllers;
+
+namespace ConsoleApplicationWithOptions
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
